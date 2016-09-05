561c49bbb42f1e4897adba8eed893c4dc1d77972
Sayed Adel <seiko@imavr.com>
2016-09-05 06:19:01 +0200

    rm m4

--- rmgen.sh
+++ rmgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname "$0"`
 make clean
-rm -rf src/.deps src/.dirstamp tests/.deps tests/.dirstamp test-driver mep_test* m4/* Makefile config.log configure config.log config.sub config.guess Makefile.in depcomp libtool missing aclocal.m4 autom4te.cache compile config.status install-sh ltmain.sh
+rm -rf src/.deps src/.dirstamp tests/.deps tests/.dirstamp test-driver mep_test* m4 Makefile config.log configure config.log config.sub config.guess Makefile.in depcomp libtool missing aclocal.m4 autom4te.cache compile config.status install-sh ltmain.sh
 
